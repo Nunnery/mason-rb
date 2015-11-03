599bc8e6f2691290a9ae14a97a2835e1fd84ece0
Richard Harrah <topplethenun@tealcubegames.com>
2015-11-03 10:24:25 -0500

    cleaning helper_spec and reducing duplication

--- spec/mason-rb/helper_spec.rb
+++ spec/mason-rb/helper_spec.rb
@@ -5,66 +5,49 @@ RSpec.describe MasonRb::Helper do
 
   # Testing MasonRb::Helper.is_ci?
   describe '.is_ci?' do
+    let(:travis) { ENV['TRAVIS'] }
+    let(:jenkins_url) { ENV['JENKINS_URL'] }
+    let(:ci) { ENV['CI'] }
+
+    after(:each) do
+      ENV['TRAVIS'] = travis
+      ENV['JENKINS_URL'] = jenkins_url
+      ENV['CI'] = ci
+    end
+
     # Testing Jenkins support because it's environment variable based
     context 'when running on Jenkins' do
-      let(:travis) { ENV['TRAVIS'] }
-      let(:jenkins_url) { ENV['JENKINS_URL'] }
-      let(:ci) { ENV['CI'] }
-      before {
+      before(:each) {
         ENV['TRAVIS'] = nil
         ENV['JENKINS_URL'] = 'http://build.tealcube.com/'
         ENV['CI'] = nil
       }
-      after {
-        ENV['TRAVIS'] = travis
-        ENV['JENKINS_URL'] = jenkins_url
-        ENV['CI'] = ci
-      }
-      let(:on_ci) { MasonRb::Helper.is_ci? }
       it 'returns true' do
-        expect(on_ci).to eq true
+        expect(MasonRb::Helper.is_ci?).to eq true
       end
     end
 
     # Testing Travis support because it's environment variable based
     context 'when running on Travis' do
-      let(:travis) { ENV['TRAVIS'] }
-      let(:jenkins_url) { ENV['JENKINS_URL'] }
-      let(:ci) { ENV['CI'] }
-      before {
+      before(:each) {
         ENV['TRAVIS'] = 'true'
         ENV['JENKINS_URL'] = nil
         ENV['CI'] = 'true'
       }
-      after {
-        ENV['TRAVIS'] = travis
-        ENV['JENKINS_URL'] = jenkins_url
-        ENV['CI'] = ci
-      }
-      let(:on_ci) { MasonRb::Helper.is_ci? }
       it 'returns true' do
-        expect(on_ci).to eq true
+        expect(MasonRb::Helper.is_ci?).to eq true
       end
     end
 
     # Testing developer workstation because it's environment variable based
     context 'running on developer workstation' do
-      let(:travis) { ENV['TRAVIS'] }
-      let(:jenkins_url) { ENV['JENKINS_URL'] }
-      let(:ci) { ENV['CI'] }
-      before {
+      before(:each) {
         ENV['TRAVIS'] = nil
         ENV['JENKINS_URL'] = nil
         ENV['CI'] = nil
       }
-      after {
-        ENV['TRAVIS'] = travis
-        ENV['JENKINS_URL'] = jenkins_url
-        ENV['CI'] = ci
-      }
-      let(:on_ci) { MasonRb::Helper.is_ci? }
       it 'returns false' do
-        expect(on_ci).to eq false
+        expect(MasonRb::Helper.is_ci?).to eq false
       end
     end
   end
@@ -73,21 +56,22 @@ RSpec.describe MasonRb::Helper do
   # This is kind of hard to test because we're already running in RSpec...
   describe '.is_test?' do
     context 'when using RSpec' do
-      let(:on_test) { MasonRb::Helper.is_test? }
       it 'returns true' do
-        expect(on_test).to eq true
+        expect(MasonRb::Helper.is_test?).to eq true
       end
     end
   end
 
+  # Testing that Pastel actually colors Strings as expected
   describe '::PASTEL' do
     # Make sure that MasonRb::Helper::PASTEL exists
     it { is_expected.to have_constant 'PASTEL' }
     let(:message) { 'Hello, world!' }
     let(:pastel) { Pastel.new }
+    let(:colored) { pastel.green message }
 
     it 'colors strings' do
-      expect(pastel.green(message)).to eq MasonRb::Helper::PASTEL.green(message)
+      expect(colored).to eq MasonRb::Helper::PASTEL.green(message)
     end
   end
 
